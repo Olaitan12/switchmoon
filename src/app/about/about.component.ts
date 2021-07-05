d8ecce74de00dbfbb196420d75319677f29b4476
Olaitan <olaitanaderoju@gmail.com>
2021-07-05 11:36:28 -0700

    configurating files

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from '../config.service';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  about = {
+    tagline: 'About Us',
+    subheading: 'SWITCHMOON NIGERIA LIMITED',
+    heading: 'SWITCHMOON NIGERIA LIMITED is a diverse information technology professional Services Company founded on the vision of providing our clients with a competitive edge. This is accomplished by delivering cost effective IT solutions, based on Open Systems and Open Source technologies, which are tailored to their strategic business considerations. Our aim is to develop strong, ongoing relationships with our clients and long-term partnerships, based on mutual growth and respect.',
+    headingtext: 'Our focus is in the following industries; Financial Services, FCMG, Healthcare, Manufacturing and Trade related entities. We cater to provide solutions to small, medium and large-scale organizations, providing solution varied but not limited to Linux systems administration and network management, productivity solutions based on web and client/server technology and platform integration via TCP/IP.'
+  };
+
+  constructor(private config: ConfigService) { }
 
   ngOnInit(): void {
+    this.about = this.getAbout();
+  }
+
+  getAbout() {
+    return this.config.getConfig().about;
   }
 
 }
--- src/app/configuration.ts
+++ src/app/configuration.ts
@@ -3,4 +3,11 @@ export const configuration = {
     heading: 'SWITCHMOON',
     headingtext: "SWITCHMOON NIGERIA LIMITED is an Information and Communication Technology consulting firm and technology solution provider. We recognize the Impact of ICT in business operations and service delivery through digital transformation. Poised to be a positive change agent in service delivery and overall business efficiency in any organization.",
     },
+
+    about: {
+      tagline: 'About Us',
+      subheading: 'SWITCHMOON NIGERIA LIMITED',
+      heading: 'SWITCHMOON NIGERIA LIMITED is a diverse information technology professional Services Company founded on the vision of providing our clients with a competitive edge. This is accomplished by delivering cost effective IT solutions, based on Open Systems and Open Source technologies, which are tailored to their strategic business considerations. Our aim is to develop strong, ongoing relationships with our clients and long-term partnerships, based on mutual growth and respect.',
+      headingtext: 'Our focus is in the following industries; Financial Services, FCMG, Healthcare, Manufacturing and Trade related entities. We cater to provide solutions to small, medium and large-scale organizations, providing solution varied but not limited to Linux systems administration and network management, productivity solutions based on web and client/server technology and platform integration via TCP/IP.'
+    },
   };
