a9dd82ee12c21770c39202bc0a1b50ea3c3155a8
Olaitan <olaitanaderoju@gmail.com>
2021-07-05 07:29:45 -0700

    Component and Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ContactComponent } from './contact/contact.component';
+import { PartnersComponent } from './partners/partners.component';
+import { CtaComponent } from './cta/cta.component';
+import { FooterComponent } from './footer/footer.component';
+import { NagivationComponent } from './nagivation/nagivation.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ServicesComponent,
+    HomeComponent,
+    NavigationComponent,
+    ContactComponent,
+    PartnersComponent,
+    CtaComponent,
+    FooterComponent,
+    NagivationComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
